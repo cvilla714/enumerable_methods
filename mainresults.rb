42bbb00bb721c22ebcb3eb83ffe03a2c603b0b7e
alan soto valle <soto.alan@hotmail.com>
2020-10-12 17:56:50 -0500

    Changed function aproach to object aproach

--- mainresults.rb
+++ mainresults.rb
@@ -7,23 +7,33 @@ cities = %w[montreal manhattan jersey atlanta hollywood]
 # array or numbers
 totals = [-10, 20, 30, 40, 50, 8, 100]
 tot_inject = [1, 3, 5, 7, 9]
-my_array = [5, 9, 3, 2, 4, 1]
+my_array = [5, 9, 3, 2, -4, 1]
 
 # calling each module
-my_each(my_array) { |index| p index * 2 }
+my_array.my_each { |index| p index * 2 }
 
-my_each_with_index(my_array) do |item, index|
+my_array.my_each_with_index do |item, index|
   p item if index == 2
 end
 
-p my_select(my_array, &:positive?)
-p my_all(my_array, &:positive?)
-p my_any(my_array, &:negative?)
-p my_none(cities) { |index| index.length > 5 }
-my_count(totals) { puts "the number of items is #{totals.length}" }
-my_map(totals) { |num| puts num * 3 }
-p my_inject(tot_inject) { [1, 3, 5, 7, 9] }
-p multiply_els(tot_inject) { [1, 3, 5, 7, 9] }
+p my_array.my_select(&:positive?)
+
+p my_array.my_all(&:positive?)
+
+p my_array.my_any(&:negative?)
+
+print cities.my_none { |index| index.length > 10 }
+
+totals.my_count { puts "the number of items is #{totals.length}" }
+
+print totals.my_map { |num| puts num * 3 }
+
+print tot_inject.my_inject { [1, 3, 5, 7, 9] }
+
+p tot_inject.multiply_els { [1, 3, 5, 7, 9] }
+
 over_forty = proc { |num| num >= 40 }
-p group_over_forty = my_select(totals, &over_forty)
-my_map_proc(totals) { p group_over_forty }
+
+group_over_forty = totals.my_select(&over_forty)
+
+totals.my_map_proc { p group_over_forty }
