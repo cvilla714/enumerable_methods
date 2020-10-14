6cf11b8ec1f84af64ef164645b1e06c8dc7ea862
alan soto valle <soto.alan@hotmail.com>
2020-10-14 16:07:28 -0500

    Fixed some problems with mu_none method

--- methods.rb
+++ methods.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # module Enumerable
-module Enumerable # rubocop:disable Metrics/ModuleLength
+module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
       arr = to_a if self.class == Range
@@ -57,33 +57,6 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
       false
     else
       'Block missing'
-    end
-  end
-
-  def my_none?
-    if !block_given?
-      my_each  do |item| 
-        if item == nil && item == [] 
-          return false
-        elsif item == nil
-          return true 
-        end
-        return false unless item == false 
-      end
-      true
-      elsif block_given?
-        # return to_enum(:my_none) unless block_given?
-        arr = to_a if self.class == Range
-        arr = self if self.class == Array
-        m = 0
-          while m < arr.length
-            if yield(arr[m])
-              return false
-            end
-            m += 1
-          end
-        true
-      end
     end
   end
 
@@ -151,4 +124,62 @@ module Enumerable # rubocop:disable Metrics/ModuleLength
       'Block missing'
     end
   end
+
+  def my_none?(*input) # if all elements are false return true
+    return true if !block_given? && self.empty?
+
+    if !block_given? 
+        my_each  do |item|
+            return false if item.class == input[0]
+        end
+    elsif !block_given? 
+        my_each  do |item|
+            char = item.split('')
+            char.my_each do |n| 
+                return false if n.match?(input[0]) 
+            end
+        end
+        true
+    elsif !block_given?
+        my_each  do |item|
+            return false if item == true # if one atleast item is true return false
+        end
+        true
+    elsif block_given?
+#        arr = to_a if self.class == Range
+#        arr = self if self.class == Array
+        my_each  do |item|
+            return false if item == true
+            return false if yield(item) 
+        end
+        true
+    end
+  end
+
 end
+
+
+
+#p [1, 2, 3, 4, 5].my_none?(&proc { |n| (n % 7).zero? }) 
+#p [1, 2, 3, 4, 5].my_none?(&proc { |n| n.even? }) 
+#p (1..3).my_none?(&proc { |num| num.even? }) 
+#p [1, 2, 3, 4, 5].tap { |t| t.my_none? { |n| n % 3 } } 
+#p [false, nil, false].my_none? 
+#p [false, nil, []].my_none? 
+#p [true, []].my_none?(String) 
+#p [true, []].my_none?(Numeric) 
+#p ['', []].my_none?(String) 
+#p %w[dog cat].my_none?(/x/) 
+#p %w[dog cat].my_none?(/d/) 
+#p %w[dog car].my_none?(5) 
+#p [5, 'dog', 'car'].my_none?(5) 
+
+#p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+#p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+p %w{ant bear cat}.my_none?(/d/)                        #=> true
+p [1, 3.14, 42].my_none?(Float)                         #=> false
+#p [].my_none?                                           #=> true
+#p [nil].my_none?                                        #=> true
+#p [nil, false].my_none?                                 #=> true
+#p [nil, false, true].my_none?                           #=> false
+
