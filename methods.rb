1c63e4c643ab3ce03fb87fb29d8d88ec0cfcffa9
Cosmel Villalobos <cosmelvg@gmail.com>
2020-10-14 18:09:11 -0400

    merging the latest version of my_none

--- methods.rb
+++ methods.rb
@@ -62,20 +62,79 @@ module Enumerable
     end
   end
 
-  def my_count
-    if block_given?
-      x = 0
-      while x < length
-        puts "this are the items insidet the array #{self[x]}"
-        x += 1
-      end
-      yield
-    else
-      'Block missing'
+  # def my_none?
+    # if !block_given?
+      # my_each do |item|
+        # if item.nil? && item == []
+          # return false
+        # elsif item == []
+          # return true
+        # elsif item.nil?
+          # return true
+        # end
+        # return false unless item == false
+      # end
+      # true
+      # end
+    # end
+    # elsif block_given?
+      # return to_enum(:my_none) unless block_given?
+      # arr = to_a if self.class == Range
+      # arr = self if self.class == Array
+      # m = 0
+      # while m < arr.length
+        # if yield(arr[m])
+        # return false 
+        # end
+        # m += 1
+      # end
+      # true
+    # end
+  # end
+
+  def my_none?(*input) # if all elements are false return true
+    return true if !block_given? && self.empty?
+
+    # if !block_given? 
+        # my_each  do |item|
+            # return false if item.class == input[0]
+        # end
+    if !block_given? 
+        my_each  do |item|
+            char = item.split('')
+            char.my_each do |n|   
+              return false if n.match?(input[0]) 
+            end
+        end
+        true
+    elsif !block_given?
+        my_each  do |item|
+            return false if item == true # if one atleast item is true return false
+        end
+        true
+    elsif block_given?
+#        arr = to_a if self.class == Range
+#        arr = self if self.class == Array
+        my_each  do |item|
+            return false if item == true
+            return false if yield(item) 
+        end
+        true
+    end
+  end
+
+
+
+def my_count
+  if block_given?
+    x = 0
+    while x < length
+      puts "this are the items insidet the array #{self[x]}"
+      x += 1
     end
     yield
-    # else
-    # 'Block missing'
+  else
+    'Block missing'
   end
 end
 
@@ -131,82 +190,63 @@ def my_map_proc
   end
 end
 
-def my_nonetwo?
-  # arr = to_a if self.class == Range
-  # arr = self if self.class == Array
-  # arg = arr
 
-  # if !block_given?
-  # my_each do |item|
-  # if item.nil? && item == []
-  # return false
-  # elsif item == []
-  # return true
-  # elsif item.nil?
-  # return true
-  # end
-  # return false unless item == false
-  # end
+  def my_nonetwo?
+    # arr = to_a if self.class == Range
+    # arr = self if self.class == Array
+    # arg = arr
 
-  unless block_given?
-    my_each do |element|
-      p element * 2
-      # return false
-    end
-    # true
-  end
-  # end
-  # elsif block_given?
-  # return to_enum(:my_none) unless block_given?
-  # arr = to_a if self.class == Range
-  # arr = self if self.class == Array
-  # m = 0
-  # while m < arr.length
-  # return false if yield(arr[m])
-  # m += 1
-  # end
-  # true
-  # end
-# end
+    # if !block_given?
+      # my_each do |item|
+        # if item.nil? && item == []
+          # return false
+        # elsif item == []
+          # return true
+        # elsif item.nil?
+          # return true
+        # end
+        # return false unless item == false
+      # end
 
-def my_none?(*input) # if all elements are false return true
-  return true if !block_given? && empty?
-
-  if !block_given?
-    my_each do |item|
-      return false if item.class == input[0]
-    end
-  elsif !block_given?
-    my_each do |item|
-      char = item.split('')
-      char.my_each do |n|
-        return false if n.match?(input[0])
+      if !block_given?
+        my_each do |element|
+          p element * 2 
+        # return false
+        end
+      # true
       end
-    end
-    true
-  elsif !block_given?
-    my_each do |item|
-      return false if item == true # if one atleast item is true return false
-    end
-    true
-  elsif block_given?
-      #        arr = to_a if self.class == Range
-      #        arr = self if self.class == Array
-      my_each do |item|
-        return false if item == true
-        return false if yield(item)
-      end
-      true
-      end
+    # end
+    # elsif block_given?
+      # return to_enum(:my_none) unless block_given?
+      # arr = to_a if self.class == Range
+      # arr = self if self.class == Array
+      # m = 0
+      # while m < arr.length
+        # return false if yield(arr[m])
+        # m += 1
+      # end
+      # true
+    # end
   end
 end
 
-# p [1, 2, 3, 4, 5].my_none?(&proc { |n| (n % 7).zero? })
-# p [1, 2, 3, 4, 5].my_none?(&proc { |n| n.even? })
-# p (1..3).my_none?(&proc { |num| num.even? })
-# p [1, 2, 3, 4, 5].tap { |t| t.my_none? { |n| n % 3 } }
-# p [false, nil, false].my_none?
-# p [false, nil, []].my_none?
+# my_none
+# p %w[ant bear cat].my_none? { |word| word.length == 5 } #=> true
+# p %w[ant bear cat].my_none? { |word| word.length >= 4 } #=> false
+p %w{ant bear cat}.my_none?(/d/)                       #=> true
+# p [1, 3.14, 42].my_none?(Float)                        #=> false
+# p [].my_none?                                           #=> true
+# p [nil].my_none?                                        #=> true
+# p [nil, false].my_none?                                 #=> true
+# p [nil, false, true].my_none?                           #=> false having problems here
+# p [1, 2, 3, 4, 5].my_none?(&proc { |n| (n % 7).zero? }) #=> true
+# p [1, 2, 3, 4, 5].my_none?(&proc { |n| n.even? }) #=> false
+# p (1..3).my_none?(&proc { |num| num.even? })            #=> false
+# p [1, 2, 3, 4, 5].tap { |t| t.my_none? { |n| n % 3 } }  # returns the same array
+# p [1, 2, 3, 4, 5].tap { |t| t.none? { |n| n % 3 } } # returns the same array
+# p [false, nil, false].my_none?                          #=> true
+# p [false, nil, []].my_none?                             #=> false problems here
+# p [1,2,3,4,5,6,7].my_nonetwo?
 # p [true, []].my_none?(String)
 # p [true, []].my_none?(Numeric)
 # p ['', []].my_none?(String)
@@ -215,11 +255,4 @@ end
 # p %w[dog car].my_none?(5)
 # p [5, 'dog', 'car'].my_none?(5)
 
-# p %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
-# p %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
-p %w[ant bear cat].my_none?(/d/) #=> true
-# p [1, 3.14, 42].my_none?(Float) #=> false
-# p [].my_none?                                           #=> true
-# p [nil].my_none?                                        #=> true
-# p [nil, false].my_none?                                 #=> true
-# p [nil, false, true].my_none?                           #=> false
+
